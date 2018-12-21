6834a7a0d791913506d58ba51759bfd752b82b0a
Mitchel Parish <mitchelparish@gmail.com>
2018-12-21 20:50:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
+  if number % 15
     "FizzBuzz"
   elsif number % 3 == 0
     "Fizz"
   elsif number % 5 == 0
     "Buzz"
   else
-    nil 
+    nil
   end
 
 end
